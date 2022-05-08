08a383939c6b2a966d821d147a54b8763aa7d827
dodo <saad9191h@gmail.com>
2022-05-07 23:44:28 -0400

    solution using just functions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,13 @@
 const reverseString = function(str) {
-    let lenOfStr = str.length;
-    let reversedStr = "";
-    for (let i = lenOfStr-1; i >= 0; i--) {
-        reversedStr = reversedStr + str[i];
-    }
+
+    let splitArray = str.split(""); //splits the string into an array with the elements being a single individual character
+    let reversedArray = splitArray.reverse();   //reverses the array
+
+    let reversedStr = reversedArray.join(""); //joins the array into a string
+
     return reversedStr;
+
+    // return str.split("").reverse().join(""); //short for above code
 };
 
 // Do not edit below this line
