ff2f065f820fa03a6eaf0c2188ffb717ca1e4e73
dodo <saad9191h@gmail.com>
2022-05-07 23:35:47 -0400

    My Solution This is my solution. Works perfectly fine. I have used for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let lenOfStr = str.length;
+    let reversedStr = "";
+    for (let i = lenOfStr-1; i >= 0; i--) {
+        reversedStr = reversedStr + str[i];
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
