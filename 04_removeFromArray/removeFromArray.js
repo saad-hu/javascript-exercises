4f93c5210c7e202777be7c009330418f9e271639
dodo <saad9191h@gmail.com>
2022-05-08 01:54:06 -0400

    solved using rest parameter, indexOf() and splice()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(userArray, ...remItems) {  //...remItems is a Rest parementer. It is an array of all the parameters that are entered after userArray parameter
 
+    nextIteration: for (let i = 0; i < remItems.length; i++) //nextIteration is a label for the for loop
+    {
+        let remIndex = userArray.indexOf(remItems[i]);
+
+        if (remIndex == -1) continue nextIteration; //indexOf method returns -1 if the parameter is not found. If it is not found, continue will skip to the next parameter to be removed from the array
+
+        userArray.splice(remIndex, 1); //splice changes the original array and returns the DELETED items. It does not returns the modified array
+    }
+
+    return userArray;
 };
 
 // Do not edit below this line
