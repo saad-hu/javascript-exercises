3bc47493e5706882c85ac19a504ea05310460dc9
dodo <saad9191h@gmail.com>
2022-05-07 22:22:46 -0400

    complete solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, repeatNum) {
+    
+    if(repeatNum < 0) return "ERROR"; //no -ve numbers allowed
 
+    let finalStr = "";      //an empty string to add the user string to
+
+    for (let i = 0; i < repeatNum; i++) {   
+        finalStr = finalStr.concat(str);
+    }
+    
+    return finalStr;
 };
 
 // Do not edit below this line
