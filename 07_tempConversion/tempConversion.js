910aeac5f28bb7eaba70a276cd2b38c1579f6a33
dodo <saad9191h@gmail.com>
2022-05-08 04:06:37 -0400

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let cel = (fah - 32) * (5/9) * 10;
+  return Math.round(cel) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let fah = (cel * (9/5) + 32) * 10;
+  return Math.round(fah) / 10;
 };
 
 // Do not edit below this line
