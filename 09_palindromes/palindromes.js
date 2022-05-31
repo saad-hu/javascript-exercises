08854aeb7546c3f60a6cd6f0dd44192746b5e7dc
dodo <saad9191h@gmail.com>
2022-05-31 08:41:04 -0400

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    //this removes all spaces and punctuation 
+    let noPunc = sentence.replaceAll(" ", "").replaceAll(".", "").replaceAll(",", "").replaceAll("!", "");
+    let noPuncLower = noPunc.toLowerCase(); //converting to lowercase
+    
+    //now we have to make a reverse string of noPuncLower
+    //Method 1: this converts the string to array. then uses reverse method on array. then join method to convert form array to string
+    //there is no direct method to reverse a string
 
+    let reverseStr = noPuncLower.split("").reverse().join("");
+
+    //Method 2: this uses loop. No methods are used
+    // let reverseStr = "";
+
+    // for(let i = noPuncLower.length - 1; i >= 0; i--) {
+    //     reverseStr = reverseStr + noPuncLower[i];
+    // }
+
+    if (noPuncLower === reverseStr) return true;
+
+    else return false;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
