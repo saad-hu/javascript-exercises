3c0d1d6277519fb1ec49f79550c367c690248fc7
dodo <saad9191h@gmail.com>
2022-05-31 12:46:50 -0400

    solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    //Implementation 1
+    // let titles = []; //creating an empty array
 
+    // for(let i = 0; i < books.length; i++) {
+    //     titles[i] = books[i].title;
+    // }
+    // return titles;
+
+    //Implementation 2
+    let titles = books.map(book => book.title);
+
+    return titles;
 };
 
 // Do not edit below this line
