9a28e6c5b7b529b6e398abfa384c3f5abc01eec8
dodo <saad9191h@gmail.com>
2022-05-31 11:08:57 -0400

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
 
+    if(member <= 0) return "OOPS"
+
+    let arrayIndex = member - 1;
+    let fibo = [1,1];   
+
+    for(let i = 2; i <= arrayIndex; i++) {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+
+    return fibo[arrayIndex];
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
