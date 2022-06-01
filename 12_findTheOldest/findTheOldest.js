1caa69dde585bed4b1109d4704c90be74c6b48df
dodo <saad9191h@gmail.com>
2022-06-01 11:55:18 -0400

    solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+  let oldestPerson = people.reduce((person, currentPerson) => {
+      let previousAge = returnAge(person.yearOfBirth, person.yearOfDeath);
+      let currentAge = returnAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+      if(currentAge > previousAge) return currentPerson;
+      else return person;
+
+  }, {})
+
+  return oldestPerson;
 };
+
+function returnAge(birthYear, deathYear) {
+  if(!birthYear) birthYear  = new Date().getFullYear();
+  if(!deathYear) deathYear = new Date().getFullYear();
+  
+  return (deathYear-birthYear);
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
