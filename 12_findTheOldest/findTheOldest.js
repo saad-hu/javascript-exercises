531bcf6a0aa5a2887ad839bcd3072f25ca71502b
dodo <saad9191h@gmail.com>
2022-06-01 12:05:34 -0400

    improved code such that now the initial value is taken to the first array element. Before it was an empty object.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,24 +1,22 @@
 const findTheOldest = function(people) {
 
-  let oldestPerson = people.reduce((person, currentPerson) => {
-      let previousAge = returnAge(person.yearOfBirth, person.yearOfDeath);
-      let currentAge = returnAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+  let oldest = people.reduce((oldestPerson, currentPerson) => {
+    let oldestAge = returnAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    let currentAge = returnAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    console.log(`${oldestAge}, ${currentAge}`);
 
-      if(currentAge > previousAge) return currentPerson;
-      else return person;
+    if(currentAge > oldestAge) return currentPerson;
+    else return oldestPerson;
+  })
 
-  }, {})
-
-  return oldestPerson;
+  return oldest;
 };
 
 function returnAge(birthYear, deathYear) {
-  if(!birthYear) birthYear  = new Date().getFullYear();
   if(!deathYear) deathYear = new Date().getFullYear();
   
   return (deathYear-birthYear);
 }
-
 
 // Do not edit below this line
 module.exports = findTheOldest;
